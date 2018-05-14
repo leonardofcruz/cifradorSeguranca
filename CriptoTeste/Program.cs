cf1fc83387867ea2ed755d6f6f2af38ad29d892f
leonardofcruz <leonardofcruz@yahoo.com.br>
2018-05-14 18:35:06 -0300

    Primeiro Commit

--- CriptoTeste/Program.cs
+++ CriptoTeste/Program.cs
@@ -17,8 +17,36 @@ namespace CriptoTeste
             string mensagemCifrada = "";
             string mensagemDecifrada = "";
 
-            IComportamento compCifra = new ComportamentoCriptografia();
-            IComportamento compDecifra = new ComportamentoDescriptografia();
+            Biblioteca compCifra = new ComportamentoCriptografia();
+            Biblioteca compDecifra = new ComportamentoDescriptografia();
+
+
+
+            Biblioteca bib;
+            if (cifra == 0)
+            {
+                bib = new Models.Biblioteca(CryptProvider.DES);
+
+            }
+            else
+            {
+                bib = new Models.Biblioteca(CryptProvider.Rijndael);
+            }
+            string _retorno = "";
+            bib.Key = this.chave;
+            if (operacao == 0)
+            {
+                _retorno = bib.Encrypt(textoOriginal);
+            }
+            else if (operacao == 1)
+            {
+                _retorno = bib.Decrypt(textoOriginal);
+            }
+            else
+            {
+                _retorno = "";
+            }
+            return _retorno;
 
 
             mensagemCifrada = compCifra.Cripto(mensagemOriginal.ToUpper(), chave, false);
